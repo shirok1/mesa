3eba9497e2c13f0d6c13d50189614847f22edc4c
Brendan King <Brendan.King@imgtec.com>
2017-12-01 08:31:15 +0000

    gbm: don't assert if DRI context creation fails
    If the DRI backend fails to create a DRI context, return an error,
    rather than asserting.

--- src/gbm/backends/dri/gbm_dri.c
+++ src/gbm/backends/dri/gbm_dri.c
@@ -1245,8 +1245,11 @@ gbm_dri_bo_map(struct gbm_bo *_bo,
    if (!dri->context)
       dri->context = dri->dri2->createNewContext(dri->screen, NULL,
                                                  NULL, NULL);
-   assert(dri->context);
    mtx_unlock(&dri->mutex);
+   if (!dri->context) {
+      errno = ENOSYS;
+      return NULL;
+   }
 
    /* GBM flags and DRI flags are the same, so just pass them on */
    return dri->image->mapImage(dri->context, bo->image, x, y,
@@ -1374,8 +1377,11 @@ gbm_dri_bo_blit(struct gbm_bo *_dst_bo, struct gbm_bo *_src_bo,
    if (!dri->context)
       dri->context = dri->dri2->createNewContext(dri->screen, NULL,
                                                  NULL, NULL);
-   assert(dri->context);
    mtx_unlock(&dri->mutex);
+   if (!dri->context) {
+      errno = ENOSYS;
+      return 0;
+   }
 
    /* GBM flags and DRI flags are the same, so just pass them on */
    dri->image->blitImage(dri->context, dst_bo->image, src_bo->image,
