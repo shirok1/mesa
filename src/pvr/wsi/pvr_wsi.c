5e48a6ba47dbcd89d0fb5594e8c6b7c7345b9b6c
shirok1 <me@shirok1.dev>
2023-02-08 04:41:54 +0800

    pvr: Use the common WSI wrappers
    Implementing <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/13234>

--- src/pvr/wsi/pvr_wsi.c
+++ src/pvr/wsi/pvr_wsi.c
@@ -26,6 +26,8 @@
 
 #include "pvr_wsi.h"
 #include "pvr_mesa_wsi_interface.h"
+#include "vk_physical_device.h"
+#include "wsi_common_entrypoints.h"
 
 VkResult
 pvr_mesa_wsi_init(struct pvr_mesa_wsi **pmwsi,
@@ -60,6 +62,11 @@ pvr_mesa_wsi_init(struct pvr_mesa_wsi **pmwsi,
    if (!sw)
       mwsi->wsi.supports_modifiers = true;
 
+   {
+      VK_FROM_HANDLE(vk_physical_device, physical_device, (mwsi->physicalDevice));
+      physical_device->wsi_device = &mwsi->wsi;
+   }
+
    *pmwsi = mwsi;
 
    return VK_SUCCESS;
@@ -69,112 +76,14 @@ void
 pvr_mesa_wsi_finish(struct pvr_mesa_wsi *mwsi,
 		    const VkAllocationCallbacks *alloc)
 {
+   {
+      VK_FROM_HANDLE(vk_physical_device, physical_device, (mwsi->physicalDevice));
+      physical_device->wsi_device = NULL;
+   }
+
    wsi_device_finish(&mwsi->wsi, alloc);
 
    vk_free(alloc, mwsi);
-}
-
-VkResult
-pvr_mesa_wsi_common_get_surface_support(struct pvr_mesa_wsi *mwsi,
-                                        uint32_t queueFamilyIndex,
-                                        VkSurfaceKHR surface,
-                                        VkBool32 *pSupported)
-{
-   return wsi_common_get_surface_support(&mwsi->wsi,
-                                         queueFamilyIndex,
-                                         surface,
-                                         pSupported);
-}
-
-VkResult
-pvr_mesa_wsi_common_get_surface_capabilities(struct pvr_mesa_wsi *mwsi,
-                                             VkSurfaceKHR surface,
-                                             VkSurfaceCapabilitiesKHR *pSurfaceCapabilities)
-{
-   return wsi_common_get_surface_capabilities(&mwsi->wsi,
-                                              surface,
-                                              pSurfaceCapabilities);
-}
-
-VkResult
-pvr_mesa_wsi_common_get_surface_capabilities2(struct pvr_mesa_wsi *mwsi,
-                                              const VkPhysicalDeviceSurfaceInfo2KHR *pSurfaceInfo,
-                                              VkSurfaceCapabilities2KHR *pSurfaceCapabilities)
-{
-   return wsi_common_get_surface_capabilities2(&mwsi->wsi,
-                                               pSurfaceInfo,
-                                               pSurfaceCapabilities);
-}
-
-VkResult
-pvr_mesa_wsi_common_get_surface_capabilities2ext(struct pvr_mesa_wsi *mwsi,
-                                                 VkSurfaceKHR surface,
-                                                 VkSurfaceCapabilities2EXT *pSurfaceCapabilities)
-{
-   return wsi_common_get_surface_capabilities2ext(&mwsi->wsi,
-                                                  surface,
-                                                  pSurfaceCapabilities);
-}
-
-VkResult
-pvr_mesa_wsi_common_get_surface_formats(struct pvr_mesa_wsi *mwsi,
-                                        VkSurfaceKHR surface,
-                                        uint32_t *pSurfaceFormatCount,
-                                        VkSurfaceFormatKHR *pSurfaceFormats)
-{
-   return wsi_common_get_surface_formats(&mwsi->wsi,
-                                         surface,
-                                         pSurfaceFormatCount,
-                                         pSurfaceFormats);
-}
-
-VkResult
-pvr_mesa_wsi_common_get_surface_formats2(struct pvr_mesa_wsi *mwsi,
-                                         const VkPhysicalDeviceSurfaceInfo2KHR *pSurfaceInfo,
-                                         uint32_t *pSurfaceFormatCount,
-                                         VkSurfaceFormat2KHR *pSurfaceFormats)
-{
-   return wsi_common_get_surface_formats2(&mwsi->wsi,
-                                          pSurfaceInfo,
-                                          pSurfaceFormatCount,
-                                          pSurfaceFormats);
-}
-
-VkResult
-pvr_mesa_wsi_common_get_surface_present_modes(struct pvr_mesa_wsi *mwsi,
-                                              VkSurfaceKHR surface,
-                                              uint32_t *pPresentModeCount,
-                                              VkPresentModeKHR *pPresentModes)
-{
-   return wsi_common_get_surface_present_modes(&mwsi->wsi,
-                                               surface,
-                                               pPresentModeCount,
-                                               pPresentModes);
-}
-
-VkResult
-pvr_mesa_wsi_common_create_swapchain(struct pvr_mesa_wsi *mwsi,
-                                     VkDevice device,
-                                     const VkSwapchainCreateInfoKHR *pCreateInfo,
-                                     const VkAllocationCallbacks *pAllocator,
-                                     VkSwapchainKHR *pSwapchain)
-{
-   return wsi_common_create_swapchain(&mwsi->wsi,
-                                      device,
-                                      pCreateInfo,
-                                      pAllocator,
-                                      pSwapchain);
-}
-
-void
-pvr_mesa_wsi_common_destroy_swapchain(UNUSED struct pvr_mesa_wsi *mwsi,
-                                      VkDevice device,
-                                      VkSwapchainKHR swapchain,
-                                      const VkAllocationCallbacks *pAllocator)
-{
-   return wsi_common_destroy_swapchain(device,
-                                       swapchain,
-                                       pAllocator);
 }
 
 VkResult
@@ -214,22 +123,10 @@ pvr_mesa_wsi_common_queue_present(struct pvr_mesa_wsi *mwsi,
                                    pPresentInfo);
 }
 
-VkResult
-pvr_mesa_wsi_common_get_present_rectangles(struct pvr_mesa_wsi *mwsi,
-                                           VkSurfaceKHR surface,
-                                           uint32_t* pRectCount,
-                                           VkRect2D* pRects)
-{
-   return wsi_common_get_present_rectangles(&mwsi->wsi,
-                                            surface,
-                                            pRectCount,
-                                            pRects);
-}
-
 /*
  * The mwsi parameter is currently unused. Note that it is invalid for
  * pvr_mesa_wsi_init, which is responsible for allocating it.
-*/
+ */
 PUBLIC void *
 pvr_mesa_wsi_sym_addr(UNUSED struct pvr_mesa_wsi *mwsi, const char *name)
 {
@@ -241,86 +138,12 @@ pvr_mesa_wsi_sym_addr(UNUSED struct pvr_mesa_wsi *mwsi, const char *name)
             pvr_mesa_wsi_init },
       { "pvr_mesa_wsi_finish",
             pvr_mesa_wsi_finish },
-      { "pvr_mesa_wsi_common_get_surface_support",
-            pvr_mesa_wsi_common_get_surface_support },
-      { "pvr_mesa_wsi_common_get_surface_capabilities",
-            pvr_mesa_wsi_common_get_surface_capabilities },
-      { "pvr_mesa_wsi_common_get_surface_capabilities2",
-            pvr_mesa_wsi_common_get_surface_capabilities2 },
-      { "pvr_mesa_wsi_common_get_surface_capabilities2ext",
-            pvr_mesa_wsi_common_get_surface_capabilities2ext },
-      { "pvr_mesa_wsi_common_get_surface_formats",
-            pvr_mesa_wsi_common_get_surface_formats },
-      { "pvr_mesa_wsi_common_get_surface_formats2",
-            pvr_mesa_wsi_common_get_surface_formats2 },
-      { "pvr_mesa_wsi_common_get_surface_present_modes",
-            pvr_mesa_wsi_common_get_surface_present_modes },
-      { "pvr_mesa_wsi_common_create_swapchain",
-            pvr_mesa_wsi_common_create_swapchain },
-      { "pvr_mesa_wsi_common_destroy_swapchain",
-            pvr_mesa_wsi_common_destroy_swapchain },
       { "pvr_mesa_wsi_common_get_images",
             pvr_mesa_wsi_common_get_images },
       { "pvr_mesa_wsi_common_acquire_next_image2",
             pvr_mesa_wsi_common_acquire_next_image2 },
       { "pvr_mesa_wsi_common_queue_present",
             pvr_mesa_wsi_common_queue_present },
-      { "pvr_mesa_wsi_common_get_present_rectangles",
-            pvr_mesa_wsi_common_get_present_rectangles },
-#if defined(VK_USE_PLATFORM_WAYLAND_KHR)
-      { "pvr_mesa_wsi_get_physical_device_wayland_presentation_support",
-            pvr_mesa_wsi_get_physical_device_wayland_presentation_support },
-      { "pvr_mesa_wsi_create_wayland_surface",
-            pvr_mesa_wsi_create_wayland_surface },
-#endif
-#if defined(VK_USE_PLATFORM_XCB_KHR)
-      { "pvr_mesa_wsi_get_physical_device_xcb_presentation_support",
-            pvr_mesa_wsi_get_physical_device_xcb_presentation_support },
-      { "pvr_mesa_wsi_create_xcb_surface",
-            pvr_mesa_wsi_create_xcb_surface },
-#endif
-#if defined(VK_USE_PLATFORM_XLIB_KHR)
-      { "pvr_mesa_wsi_create_xlib_surface",
-            pvr_mesa_wsi_create_xlib_surface },
-#endif
-      { "pvr_mesa_wsi_display_get_physical_device_display_properties",
-            pvr_mesa_wsi_display_get_physical_device_display_properties },
-      { "pvr_mesa_wsi_display_get_physical_device_display_properties2",
-            pvr_mesa_wsi_display_get_physical_device_display_properties2 },
-      { "pvr_mesa_wsi_display_get_physical_device_display_plane_properties",
-            pvr_mesa_wsi_display_get_physical_device_display_plane_properties },
-      { "pvr_mesa_wsi_display_get_physical_device_display_plane_properties2",
-            pvr_mesa_wsi_display_get_physical_device_display_plane_properties2 },
-      { "pvr_mesa_wsi_display_get_display_plane_supported_displays",
-            pvr_mesa_wsi_display_get_display_plane_supported_displays },
-      { "pvr_mesa_wsi_display_get_display_mode_properties",
-            pvr_mesa_wsi_display_get_display_mode_properties },
-      { "pvr_mesa_wsi_display_get_display_mode_properties2",
-            pvr_mesa_wsi_display_get_display_mode_properties2 },
-      { "pvr_mesa_wsi_display_create_display_mode",
-            pvr_mesa_wsi_display_create_display_mode },
-      { "pvr_mesa_wsi_get_display_plane_capabilities",
-            pvr_mesa_wsi_get_display_plane_capabilities },
-      { "pvr_mesa_wsi_get_display_plane_capabilities2",
-            pvr_mesa_wsi_get_display_plane_capabilities2 },
-      { "pvr_mesa_wsi_create_display_surface",
-            pvr_mesa_wsi_create_display_surface },
-      { "pvr_mesa_wsi_release_display",
-            pvr_mesa_wsi_release_display },
-#if defined(VK_USE_PLATFORM_XLIB_XRANDR_EXT)
-      { "pvr_mesa_wsi_acquire_xlib_display",
-            pvr_mesa_wsi_acquire_xlib_display },
-      { "pvr_mesa_wsi_get_randr_output_display",
-            pvr_mesa_wsi_get_randr_output_display },
-#endif
-      { "pvr_mesa_wsi_display_power_control",
-            pvr_mesa_wsi_display_power_control },
-      { "pvr_mesa_wsi_register_device_event",
-            pvr_mesa_wsi_register_device_event },
-      { "pvr_mesa_wsi_register_display_event",
-            pvr_mesa_wsi_register_display_event },
-      { "pvr_mesa_wsi_get_swapchain_counter",
-            pvr_mesa_wsi_get_swapchain_counter },
    };
    unsigned i;
 
